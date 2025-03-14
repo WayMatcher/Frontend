7e9fa9b167ab1b1c6d0cff7eac6ad8fdabc76af1
Niklas Wonnebauer <wonnebauer.salkinxd@gmail.com>
2025-03-14 22:20:09 +0100

    Refactor authentication API: extract common API logic, implement user registration, and enhance error handling

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import { api, handleApiError } from './common';
 import bcrypt from 'bcryptjs';
 
 import APIResponse, { MFAResponse } from '../types/API';
@@ -15,57 +15,14 @@ import User, { UserLogin } from '../types/dto/User';
 // 8. If correct, the backend sends a JWT to the frontend
 // 9. Frontend stores the JWT in the local storage
 
-
-const api: AxiosInstance = axios.create({
-    baseURL: 'https://waymatcher.com/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
-
-// Add a request interceptor
-api.interceptors.request.use(
-    (config) => {
-        const user = localStorage.getItem('user');
-        const token = user ? JSON.parse(user).token : "";
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
-
-
-
-/**
- * Authenticates a user with the provided credentials.
- *
- * @param credentials - The login credentials containing either a username or email and a password.
- * @returns A promise that resolves to a `LoginResponse` object containing authentication details.
- * @throws Will throw an error if the authentication request fails.
- */
-export const authUser = async (credentials: UserLogin): Promise<APIResponse> => {
-    try {
-        const response = await api.post('/auth/login', { user: credentials.username, password: bcrypt.hashSync(credentials.password, 10) });
-
-        return response.data as APIResponse;
-    } catch (error) {
-        handleApiError(error);
-        throw error;
-    }
-};
-
 /**
  * Authenticates a user using multi-factor authentication (MFA).
- *
- * @param user - The user object containing user details.
- * @param mfaToken - The MFA token provided by the user.
- * @returns A promise that resolves to an MFAResponse object.
- * @throws Will throw an error if the API request fails.
- */
+*
+* @param user - The user object containing user details.
+* @param mfaToken - The MFA token provided by the user.
+* @returns A promise that resolves to an MFAResponse object.
+* @throws Will throw an error if the API request fails.
+*/
 export const mfaAuthUser = async (user: User, mfaToken: string): Promise<MFAResponse> => {
     try {
         const response = await api.post('/auth/mfa', { user: user, mfaToken: bcrypt.hash(mfaToken, 10) });
@@ -77,50 +34,21 @@ export const mfaAuthUser = async (user: User, mfaToken: string): Promise<MFAResp
 }
 
 /**
- * Handles errors from API requests, specifically those made using Axios.
- * Differentiates between various types of errors (e.g., network issues, server responses, request setup errors)
- * and logs appropriate messages to the console. Throws an error with a relevant message based on the type of error.
+ * Authenticates a user with the provided credentials.
  *
- * @param error - The error object to handle. Can be an Axios error or a generic error.
- *
- * @throws {Error} Throws an error with a specific message based on the type of error encountered.
+ * @param credentials - The login credentials containing either a username or email and a password.
+ * @returns A promise that resolves to a `LoginResponse` object containing authentication details.
+ * @throws Will throw an error if the authentication request fails.
  */
-const handleApiError = (error: unknown) => {
-    if (axios.isAxiosError(error)) {
-        // Handle Axios-specific errors (e.g., network issues, timeouts)
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.error("API Error:", error.response.status, error.response.data);
+const authUser = async (credentials: UserLogin): Promise<APIResponse> => {
+    try {
+        const response = await api.post('/auth/login', { user: credentials.username, password: bcrypt.hashSync(credentials.password, 10) });
 
-            // Example: Check for specific status codes and handle them
-            if (error.response.status === 401) {
-                // Unauthorized - Redirect to login or show an error message
-                console.warn("Unauthorized: Please log in again.");
-                // You might want to clear the user context here: setUser(null);
-            } else if (error.response.status === 400) {
-                console.warn("Bad Request: Check your data");
-            } else if (error.response.status === 404) {
-                // Not Found - Handle appropriately (e.g., display a 404 message)
-                console.warn("Resource not found.");
-            } else {
-                // Other server errors (5xx)
-                console.error("Server Error:", error.response.status, error.response.data);
-            }
-            throw new Error(error.response.data.message || "An error occurred with your request");
-
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.error("Network Error: No response received.", error.request);
-            throw new Error("Network Error: Could not connect to the server.");
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.error("Request Setup Error:", error.message);
-            throw new Error("An unexpected error occurred. Please try again later.");
-        }
-    } else {
-        // Handle non-Axios errors (e.g., errors in your code)
-        console.error("Generic Error:", error);
-        throw new Error("An unexpected error occurred. Please try again later.");
+        return response.data as APIResponse;
+    } catch (error) {
+        handleApiError(error);
+        throw error;
     }
 };
+
+export default authUser;
---
+++ src/api/register.ts
@@ -0,0 +1,37 @@
+import { api, handleApiError } from './common';
+import APIResponse from '../types/API';
+
+import { UserRegister } from '../types/dto/User';
+import Address from '../types/dto/Address';
+import Vehicle from '../types/dto/Vehicle';
+
+import bcrypt from 'bcryptjs';
+
+// * How the registration flow works:
+// 1. User enters registration details in frontend
+// 2. Frontend sends registration details to the backend
+// 3. Backend checks if the registration details are correct
+// 4. If correct, the backend sends a JWT to the frontend
+// 5. Frontend stores the JWT in the local storage
+
+/**
+ * Registers a new user with the provided details.
+ *
+ * @param user - The user object containing user details.
+ * @param address - The address object containing address details.
+ * @param vehicle - The vehicle object containing vehicle details.
+ * @returns A promise that resolves to an APIResponse object.
+ * @throws Will throw an error if the API request fails.
+ */
+export const registerUser = async (user: UserRegister, address: Address, vehicle: Vehicle): Promise<APIResponse> => {
+    const hashedUser = { ...user, password: bcrypt.hashSync(user.password, 10) };
+    try {
+        const response = await api.post('/register', { hashedUser, address, vehicle });
+        return response.data as APIResponse;
+    } catch (error) {
+        handleApiError(error);
+        throw error;
+    }
+}
+
+export default registerUser;
